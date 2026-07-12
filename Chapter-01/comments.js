a7f73be9813336dba4a10b4e4933bf78a83ecbdd
[IshrathMahzhabinKhan](https://github.com/IshrathMahzhabinKhan) <officialishrath30@gmail.com>
2026-07-12 12:33:47 +0600

    Completed comments in chapter 1

---
+++ Chapter-01/comments.js
@@ -0,0 +1,12 @@
+// ==========================================
+// Chapter 1 - JavaScript Comments
+// ==========================================
+
+// This is a single-line comment.
+
+/*
+This is a multi-line comment.
+It can span multiple lines.
+*/
+
+console.log("Comments do not print.");
